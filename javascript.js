cb4c97c71e21fef6aa3b83d503b6c581a1626d57
Jessica Garber <jgarber07@gmail.com>
2019-07-20 08:44:51 -0400

    Hamburger menu works!

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
-//Fade in message at top of each page
-$('.banner').hide().delay(500).fadeIn(3500);
-//make radio buttons on contact page un-clickable
+
+//make radio buttons on contact page un-clickable -- I got this code off the internet -- I didn't write it mysef//
 $('input[type=radio]').click(function(){
     if (this.previous) {
         this.checked = false;
@@ -11,22 +10,20 @@ $('input[type=radio]').click(function(){
 
 //Creating a response to clicking 'submit' on contact page
    //create variables
-   var $firstName = $('.firstName').val("");
-   var $lastName = $('.lastName').val("");
-   var $method = $('');
-   var $time = $('');
-   const $reveal = $( 'Thank you, '+ firstName + 'Kayla will contact you via ' + method + time + '.');
+//    var $firstName = $('.firstName').val("");
+//    var $lastName = $('.lastName').val("");
+//    var $method = $('');
+//    var $time = $('');
+//  /*  const $reveal = $( 'Thank you, '+ $firstName + 'Kayla will contact you via ' + method + time + '.');*/
 
-   //print text and variables to page
-  $('.reveal').print();
+//    //print text and variables to page
+//   $('.reveal').print();
 
-   //hide confirmation text until button is clicked
-    $('.reveal').hide();
+//    //hide confirmation text until button is clicked
+//     $('.reveal').hide();
 
-   //hide button when button is pressed
-    $('.button').click(function() {
-         $('.button').hide();
-         $('.reveal').show();
-       }
-
-
+//    //hide button when button is pressed
+//     $('.button').click(function() {
+//          $('.button').hide();
+//          $('.reveal').show();
+//        })
